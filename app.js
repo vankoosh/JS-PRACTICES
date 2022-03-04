50630263f59848d82f74794e6018e9869a93961e
vankoosh <vankoosh@users.noreply.github.com>
2022-03-04 14:14:39 -0100

    async await practice lesson

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const firstPromise = (location) => {
       console.log(`Making request to ${location}. Please wait...`);
       resolve(`My location is ${location}`);
     } else {
-      reject("My location is unknown");
+      reject("My location is not known");
     }
   });
 };
@@ -16,11 +16,28 @@ const secondPromise = (firstResolved) => {
   });
 };
 
-firstPromise("Google")
-  .then((response) => {
-    console.log(`Response received as: ${response}`);
-    return secondPromise(response);
-  })
-  .then((secondResponse) => {
+// firstPromise("Google")
+//   .then((response) => {
+//     console.log(`Response received as: ${response}`);
+//     return secondPromise(response); // IN ORDER TO CHAIN PROMISES, YOU HAVE TO return THE SECOND PROMISE AND PASS IN
+//   })                                // THE resolve VALUES OF THE FIRST PROMISE AS THE PARAMETER FOR THE SECOND PROMISE
+//   .then((secondResponse) => {
+//     console.log(secondResponse);
+//   })
+//    .catch(error => {console.log(error)});
+
+
+
+// INSTEAD DO async await SYNTAX
+
+const doThis = async (msg) => {
+  try {const response = await firstPromise(msg);
+  console.log(`Response received as: ${response}`);
+  const secondResponse = await secondPromise(response);
     console.log(secondResponse);
-  });
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+doThis('Facebook');
