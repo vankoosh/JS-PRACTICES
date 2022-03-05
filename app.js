826269827e8fc4925793418fea02a4001a3c7671
vankoosh <vankoosh@users.noreply.github.com>
2022-03-05 13:08:39 -0100

    another promise excersise

--- app.js
+++ app.js
@@ -30,14 +30,58 @@ const secondPromise = (firstResolved) => {
 
 // INSTEAD DO async await SYNTAX
 
-const doThis = async (msg) => {
-  try {const response = await firstPromise(msg);
-  console.log(`Response received as: ${response}`);
-  const secondResponse = await secondPromise(response);
-    console.log(secondResponse);
-  } catch (error) {
-    console.log(error)
+// const doThis = async (msg) => {
+//   try {const response = await firstPromise(msg);
+//   console.log(`Response received as: ${response}`);
+//   const secondResponse = await secondPromise(response);
+//     console.log(secondResponse);
+//   } catch (error) {
+//     console.log(error)
+//   }
+// }
+
+// doThis('Facebook');
+
+
+const promiseOne = location => {
+  return new Promise((resolve, reject) => {
+    if (location === 'Google') {
+      console.log(`Location resolved to ${location}`)
+      resolve(`LOCATION = ${location}`)
+    } else {
+      reject('My location could not be determined');
+    }
+  }
+  )
+}
+
+const promiseTwo = responseFromPromiseOne => {
+  return new Promise((resolve, reject) => {
+    resolve(`The response from promise One is: ${responseFromPromiseOne}`)
+  })
+}
+
+// promiseOne('Google')
+//   .then(responseOne => {
+//   console.log("Resolving promise One");
+//   return promiseTwo(responseOne)
+//   })
+//   .then(responseTwo => {
+//     console.log("Resolving promise Two");
+//     console.log(responseTwo)
+//   }).catch(err => {
+//         console.log(err)
+//   })
+      
+const bothPromises = async (msg) => {
+  try {const responseOne = await promiseOne(msg);
+  console.log('Resolving promise One');
+  console.log(responseOne);
+  const responseTwo = await promiseTwo(responseOne);
+    console.log('Resolving promise Two');
+  } catch(err) {
+     console.log(err)
   }
 }
 
-doThis('Facebook');
+bothPromises('Google');
