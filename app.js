6803a53577fc66fa983b0908d5e577e80f59be6e
vankoosh <vankoosh@users.noreply.github.com>
2022-03-30 22:17:24 -0100

    another API excercise

--- app.js
+++ app.js
@@ -233,3 +233,44 @@ const sendData = () => {
 
 getBtn.addEventListener("click", getData); // no () after function name
 sendBtn.addEventListener("click", sendData);
+
+// new exccercise
+
+// Information to reach API
+const apiKey = 'b267247d6fbe4b80bc3fc900c328b31c';
+const url = 'https://api.rebrandly.com/v1/links';
+
+// Some page elements
+const inputField = document.querySelector('#input');
+const shortenButton = document.querySelector('#shorten');
+const responseField = document.querySelector('#responseField');
+
+// AJAX functions
+const shortenUrl = () => {
+  const urlToShorten = inputField.value;
+  const data = JSON.stringify({destination: urlToShorten});
+  const xhr = new XMLHttpRequest();
+  xhr.responseType = "json";
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+  renderResponse(xhr.response);
+  }
+}
+xhr.open("POST",url);
+xhr.setRequestHeader('Content-type', 'application/json');
+xhr.setRequestHeader('apikey', apiKey);
+xhr.send(data);
+}
+
+
+// Clear page and call AJAX functions
+const displayShortUrl = (event) => {
+  event.preventDefault();
+  while(responseField.firstChild){
+    responseField.removeChild(responseField.firstChild);
+  }
+  shortenUrl();
+}
+
+shortenButton.addEventListener('click', displayShortUrl);
+
